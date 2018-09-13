03bcd80e0f703ff843d484ddd0e2d4a5bf5ca7e2
RLBoy <pousseur.quentin1997@gmail.com>
2018-09-13 18:03:41 +0200

    Essaie d'une modification

--- views/accueil.php
+++ views/accueil.php
@@ -1,9 +1,9 @@
-<section >
+<section>
 			<h2>Accueil</h2>
             <form action="index.php?action=accueil" method="post" name="sendMessage">
 			<label for="message">Entrez votre message :</label>
             <input type="text" id="message" name="message">
             <input type="submit" value="envoyer" name="send">
             </form>
-            <p><?php echo $notification ?></p>
-		</section>
+            <p><?php echo $notification ?></p>    
+</section>
