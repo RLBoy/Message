b48b2b3c78e5f00bbc96d8ce562765694b334a42
RLBoy <pousseur.quentin1997@gmail.com>
2018-09-13 21:20:29 +0200

    Récupération d'un sms et envoie OK

--- controllers/AccueilController.php
+++ controllers/AccueilController.php
@@ -2,6 +2,9 @@
 
         require 'TwiTwi/Twilio/autoload.php';
         use Twilio\Rest\Client;
+        // this line loads the library
+        require('TwiTwi/Services/Twilio.php');
+
 class AccueilController{
 	
 	public function __construct() {
@@ -13,7 +16,7 @@ class AccueilController{
         
         $notification = '';
         
-
+        
         // Your Account SID and Auth Token from twilio.com/console
         $account_sid = 'AC613928773dad56b0d9e2b979109fa81d';
         $auth_token = '7334385bf3537814482a6f7f7f53f9e8';
@@ -23,17 +26,39 @@ class AccueilController{
         // A Twilio number you own with SMS capabilities
         $twilio_number = "+32460207717";
 
+        
         $client = new Client($account_sid, $auth_token);
         
-    if(!empty($_POST['send'])){
+        $messages = $client->messages->stream(
+          array( 
+          'dateSentAfter' => '2015-05-01', 
+          'dateSentBefore' => '2019-06-01'
+          )
+        );
+        
+        $messag = array();
+        
+        foreach($messages as $i){
+            
+            if($i->from != "+32460207717"){
+                var_dump($i->from);
+                $messag [] = $i;
+            }
+        }
+        
+//        var_dump($messag);
+        
+        
+        
+        if(!empty($_POST['send'])){
             if(!empty($_POST['message'])){
-                $message = $_POST['message'];
+                $messages = $_POST['message'];
                $client->messages->create(
             // Where to send a text message (your cell phone?)
             '+32496391088',
             array(
                 'from' => $twilio_number,
-                'body' => $message
+                'body' => $messages
             )
         ); 
                 $notification = 'ok';
@@ -41,7 +66,7 @@ class AccueilController{
                $notification = 'pas de message'; 
             }
         
-    }   
+        }   
         
 		
 		# Un contrôleur se termine en écrivant une vue
--- views/accueil.php
+++ views/accueil.php
@@ -5,5 +5,19 @@
             <input type="text" id="message" name="message">
             <input type="submit" value="envoyer" name="send">
             </form>
-            <p><?php echo $notification ?></p>    
+            <p><?php echo $notification ?></p> 
+    
+            <?php foreach($messag as $sms) { ?> 
+          
+            <?php echo $sms->sid ; ?>
+            <?php echo $sms->from ; ?>
+            <?php echo $sms->to ; ?>
+            <?php echo $sms->dateSent->format('Y-m-d H:i:s') ; ?>
+            <?php echo $sms->status ; ?>
+            <?php echo $sms->direction ; ?>
+            <?php echo $sms->price ; ?>
+            <p><?php echo $sms->body ?></p>
+        
+          
+            <?php } ?>
 </section>
